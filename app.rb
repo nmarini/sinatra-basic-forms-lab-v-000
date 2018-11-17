2d78fdfeb0ca0a0a87054229e812705df5a983f8
Nicolas Marini <nmarini44@gmail.com>
2018-11-17 23:14:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,9 @@ class App < Sinatra::Base
   end
 
   post '/puppy' do
-    erb :display_puppy  
+    @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
+    
+    erb :display_puppy
   end
 
 end
